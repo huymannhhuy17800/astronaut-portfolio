3bf48030581b654da5b7a65ff242ddb3c067706a
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2025-07-05 09:38:54 +0700

    add testimonial section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import About from "./sections/about";
 import Hero from "./sections/hero";
 import NavBar from "./sections/navbar";
 import Projects from "./sections/projects";
+import Testimonial from "./sections/testimonial";
 import WorkExperience from "./sections/work-experiences";
 
 function App() {
@@ -19,7 +20,10 @@ function App() {
       {/* experience */}
       <WorkExperience />
       {/* testimonial */}
+      <Testimonial />
       {/* contact */}
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
       {/* footer */}
     </div>
   );
---
+++ src/components/marquee.tsx
@@ -0,0 +1,74 @@
+import { twMerge } from "tailwind-merge";
+import { ComponentPropsWithoutRef } from "react";
+
+interface MarqueeProps extends ComponentPropsWithoutRef<"div"> {
+  /**
+   * Optional CSS class name to apply custom styles
+   */
+  className?: string;
+  /**
+   * Whether to reverse the animation direction
+   * @default false
+   */
+  reverse?: boolean;
+  /**
+   * Whether to pause the animation on hover
+   * @default false
+   */
+  pauseOnHover?: boolean;
+  /**
+   * Content to be displayed in the marquee
+   */
+  children: React.ReactNode;
+  /**
+   * Whether to animate vertically instead of horizontally
+   * @default false
+   */
+  vertical?: boolean;
+  /**
+   * Number of times to repeat the content
+   * @default 4
+   */
+  repeat?: number;
+}
+
+export function Marquee({
+  className,
+  reverse = false,
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
+  ...props
+}: MarqueeProps) {
+  return (
+    <div
+      {...props}
+      className={twMerge(
+        `group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)] ${
+          vertical ? "flex-col" : "flex-row"
+        }`,
+        className
+      )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={twMerge(
+              `flex shrink-0 justify-around [gap:var(--gap)] ${
+                vertical
+                  ? "animate-marquee-vertical flex-col"
+                  : "animate-marquee flex-row"
+              } ${
+                pauseOnHover && "group-hover:[animation-play-state:paused]"
+              } ${reverse && "[animation-direction:reverse]"}`
+            )}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
+  );
+}
